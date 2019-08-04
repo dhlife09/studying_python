2737d876400a3654ba516d746005cdf6dc2fe042
DH <dhlife09@gmail.com>
2019-08-04 14:32:06 +0900

    Create 2_variable.py

---
+++ 2_variable.py
@@ -0,0 +1,9 @@
+print('안녕! 나는 한국인이야. 나는 다리가 2개 있어.')
+print('안녕! 나는', '한국인', '이야. 나는 다리가 ' '2', '개 있어.')
+
+identity = '한국인'
+number_of_legs = 2
+print('안녕! 나는', identity, '이야, 나는 다리가', number_of_legs, '개 있어.')
+
+season = '여름'
+print('지금은', season, '입니다!')
