36686839a1b484b7c72c48ef2dde6fa4e1e51ea9
DH <dhlife09@gmail.com>
2019-08-04 15:15:20 +0900

    Create 5_if.py

---
+++ 5_if.py
@@ -0,0 +1,30 @@
+people = 20
+apple = 20
+
+if people < apple / 5: #people [3] < apple[20]/5=[4]
+    print('신나는 사과 파티! 배 터지게 먹자!')
+
+if apple % people > 0:
+    print('사과 수가 맞지 않아! 몇 개는 쪼개 먹자!')
+
+if people > apple:
+    print('사람이 너무 많아! 몇 명은 못 먹겠네.... :(')
+
+if people == apple:
+    print('모두 다 하나씩 먹으면 되겠네!')
+
+if people != apple: #같지 않을 때 != <<< 이렇게 표현함!
+    print('사과의 수와 사람의 수는 다르군')
+
+if apple == 0:
+    print('사과가 멸종했나보군.')
+
+"""
+
+if True:
+    print('조건식이 True')
+
+if False:
+    print('조건식이 False')
+
+"""
