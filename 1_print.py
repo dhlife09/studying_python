b0928aaa571c867a4470fb47934ad0c8e3676f1c
DH <dhlife09@gmail.com>
2020-01-01 22:42:14 +0900

    Update 1_print.py

--- 1_print.py
+++ 1_print.py
@@ -1,12 +1,12 @@
 print('Hello World!')
-print('Hello. I am Korean!')
-print('파이썬 쉽네요 헷')
+print('Happy New Year! 2020')
+print('파이썬 쉽네요 ㅎㅎㅎㅎ')
 print('이렇게 하면 다음줄인가? \n정답!')
 
 #이건 주석
 #Hello World 문장 3번 출력하기
 
-print('C언어랑 조금 헤깔리네 ㅋㅋ; \n')
+print('first \nsecond')
 
 print('Hello World')
 print('Hello World')
